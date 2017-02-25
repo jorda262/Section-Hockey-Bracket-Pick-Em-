2e96ae311cf0897c561ef100ca6b86f4c676e8f5
jorda262 <jorda262@icloud.com>
2017-02-24 19:18:32 -0600

    Bracket
    Bracket PHP
    Bracket CSS

---
+++ Project/bracket.php
@@ -0,0 +1,72 @@
+<head>
+	<link rel = "stylesheet" href = "bracket.css">
+</head>
+<body>
+<main id="tournament">
+	<ul class="round round-1">
+		<li class="spacer">&nbsp;</li>
+		<li class="spacer">&nbsp;</li>
+
+		<li class="game game-top winner"><span></span></li>
+		<li class="game game-spacer">&nbsp;</li>
+		<li class="game game-bottom "><span></span></li>
+
+		<li class="spacer">&nbsp;</li>
+		<li class="spacer">&nbsp;</li>
+
+		<li class="game game-top winner"><span></span></li>
+		<li class="game game-spacer">&nbsp;</li>
+		<li class="game game-bottom "><span></span></li>
+
+		<li class="spacer">&nbsp;</li>
+		<li class="spacer">&nbsp;</li>
+
+		<li class="game game-top "><span></span></li>
+		<li class="game game-spacer">&nbsp;</li>
+		<li class="game game-bottom winner"><span></span></li>
+
+		<li class="spacer">&nbsp;</li>
+		<li class="spacer">&nbsp;</li>
+
+		<li class="game game-top winner"><span></span></li>
+		<li class="game game-spacer">&nbsp;</li>
+		<li class="game game-bottom "><span></span></li>
+
+		<li class="spacer">&nbsp;</li>
+		<li class="spacer">&nbsp;</li>
+	</ul>
+	<ul class="round round-2">
+		<li class="spacer">&nbsp;</li>
+		<li class="spacer">&nbsp;</li>
+
+		<li class="game game-top winner"><span></span></li>
+		<li class="game game-spacer">&nbsp;</li>
+		<li class="game game-bottom "><span></span></li>
+
+		<li class="spacer">&nbsp;</li>
+		<li class="spacer">&nbsp;</li>
+
+		<li class="game game-top winner"><span></span></li>
+		<li class="game game-spacer">&nbsp;</li>
+		<li class="game game-bottom "><span></span></li>
+
+		<li class="spacer">&nbsp;</li>
+		<li class="spacer">&nbsp;</li>
+
+	</ul>
+	<ul class="round round-3">
+		<li class="spacer">&nbsp;</li>
+		<li class="spacer">&nbsp;</li>
+
+		<li class="game game-top winner"><span></span></li>
+		<li class="game game-spacer">&nbsp;</li>
+		<li class="game game-bottom "><span></span></li>
+
+		<li class="spacer">&nbsp;</li>
+		<li class="spacer">&nbsp;</li>
+	</ul>
+	<ul class="round round-4">
+		<li class="game game-top winner"><span></span></li>
+	</ul>
+</main>
+</body>
